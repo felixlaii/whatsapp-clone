8c3a6f8abe2441687c6603d6ceb10bc0bd479b07
Felix Lai <felix.lai@hotmail.com>
2022-11-21 21:50:21 -0500

    Update SideBar.js

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 
-
 function SideBar() {
-  return (
-    <div>SideBar</div>
-  )
+  const [allUsers, setAllUsers] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [friendList, setFriendList] = useState([]);
+  return <div>SideBar</div>;
 }
 
-export default SideBar
+export default SideBar;
