f90edc7e4d25d6073e13731f619914f8ed7fb0c5
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:11:50 -0500

    import SideBar into home and imported images

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,12 @@ import SideBar from '../SideBar/SideBar'
 function Home({ currentUser, signOut }) {
   return (
     <div className='home'>
-        
+        <div className='home-container'>
+            <SideBar currentUser={currentUser} signOut={signOut} />
+            <div className='home-bg'>
+                
+            </div>
+        </div>
     </div>
   )
 }
