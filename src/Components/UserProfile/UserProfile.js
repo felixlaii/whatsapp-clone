863467b1d69e1e041237f43daad4ed7779fb6ff1
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:23:06 -0500

    Update UserProfile.js

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function UserProfile() {
+function UserProfile({ name, photoURL, email, lastMessage }) {
   const navigate = useNavigate();
   const goToUser = (emailId) => {
     if (emailId) {
