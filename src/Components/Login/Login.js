f878afdb7c7eb82a8ae3ac376fb82bec2eb13bd3
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:15:30 -0500

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,17 @@ import "./login.scss";
 
 function Login({ setUser }) {
     const navigate = useNavigate();
+    const signInWithGoogle = () => {
+        auth
+        .signInWithPopup(googleProvider)
+        .then((result) => {
+            const newUser = {
+                fullname: result.user.displayName,
+                email: result.user.email,
+                photoURL: result.user.photoURL,
+            };
+        })
+    }
   return (
     <div>Login</div>
   )
