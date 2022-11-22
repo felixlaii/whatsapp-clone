3e87799179dd65e80d0aafe2d46d009379d463f3
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:02:23 -0500

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import "./home.scss"
+import SideBar from '../SideBar/SideBar'
 
-function Home() {
+function Home({ currentUser, signOut }) {
   return (
     <div>Home</div>
   )
