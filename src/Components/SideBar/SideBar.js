71baf8706db8fd56fd52d0f3e38eb63ab8536d9e
Felix Lai <felix.lai@hotmail.com>
2022-11-21 21:52:15 -0500

    Update SideBar.js

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,3 +1,4 @@
+import { DoorBack } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 
@@ -5,6 +6,17 @@ function SideBar({ currentUser, signOut }) {
   const [allUsers, setAllUsers] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [friendList, setFriendList] = useState([]);
+
+  useEffect(() => {
+      const getAllUsers = async () => {
+          const data = await db.collection("users").onSnapshot((snapshot) => {
+              setAllUsers(
+                  snapshot.docs.filter((doc) => doc.data().email !== currentUser?.email)
+              )
+          })
+      }
+  })
+
   return <div>SideBar</div>;
 }
 
