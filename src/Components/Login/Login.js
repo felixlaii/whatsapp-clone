3a5b3dfb6d563b85d0fb680e7ee38be998dbc612
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:18:57 -0500

    Update Login.js

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React from "react";
 import db, { auth, googleProvider } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 import "./login.scss";
 
 function Login({ setUser }) {
-    const navigate = useNavigate();
-    const signInWithGoogle = () => {
-        auth
-        .signInWithPopup(googleProvider)
-        .then((result) => {
-            const newUser = {
-                fullname: result.user.displayName,
-                email: result.user.email,
-                photoURL: result.user.photoURL,
-            };
-            navigate("/");
-            setUser(newUser);
-            localStorage.setItem("user", JSON.stringify(newUser));
-            db.collection("user").doc(result.user.email).set(newUser);
-        })
-        .catch((err) => alert(err.message));
-    }
+  const navigate = useNavigate();
+  const signInWithGoogle = () => {
+    auth
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        const newUser = {
+          fullname: result.user.displayName,
+          email: result.user.email,
+          photoURL: result.user.photoURL,
+        };
+        navigate("/");
+        setUser(newUser);
+        localStorage.setItem("user", JSON.stringify(newUser));
+        db.collection("user").doc(result.user.email).set(newUser);
+      })
+      .catch((err) => alert(err.message));
+  };
   return (
-    <div className='login'>
-        
+    <div className="login">
+      <div className="login-container">
+        <img className="login-logo" src="./whatsapp-logo.png" alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
