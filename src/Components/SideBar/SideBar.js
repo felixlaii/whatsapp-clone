41ba1203dbc2bcb49397b320bd3cbac12a3fc7cb
Felix Lai <felix.lai@hotmail.com>
2022-11-21 21:56:54 -0500

    Update SideBar.js

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -15,17 +15,17 @@ function SideBar({ currentUser, signOut }) {
         );
       });
     };
-  });
 
-  const getFriends = async () => {
-    const data = await db
-      .collection("Friendlist")
-      .doc(currentUser.email)
-      .collection("list")
-      .onSnapshot((snapshot) => {
-        setFriendList(snapshot.docs);
-      });
-  };
+    const getFriends = async () => {
+      const data = await db
+        .collection("Friendlist")
+        .doc(currentUser.email)
+        .collection("list")
+        .onSnapshot((snapshot) => {
+          setFriendList(snapshot.docs);
+        });
+    };
+  });
 
   return <div>SideBar</div>;
 }
