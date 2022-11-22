0db6553014fcaf8e5893b222941fddfb64470410
Felix Lai <felix.lai@hotmail.com>
2022-11-21 22:12:07 -0500

    Update Home.js

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import "./home.scss"
-import SideBar from '../SideBar/SideBar'
+import React from "react";
+import "./home.scss";
+import SideBar from "../SideBar/SideBar";
 
 function Home({ currentUser, signOut }) {
   return (
-    <div className='home'>
-        <div className='home-container'>
-            <SideBar currentUser={currentUser} signOut={signOut} />
-            <div className='home-bg'>
-                
-            </div>
+    <div className="home">
+      <div className="home-container">
+        <SideBar currentUser={currentUser} signOut={signOut} />
+        <div className="home-bg">
+          <img src="./WhatsAppbg.png" alt="" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
