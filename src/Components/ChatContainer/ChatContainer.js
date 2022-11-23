aa06b7dda1dadec9a1b9d0da11c5d0f43ff93c1c
Felix Lai <felix.lai@hotmail.com>
2022-11-23 10:51:56 -0500

    Update ChatContainer.js

---
+++ src/Components/ChatContainer/ChatContainer.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Chatcontainer.css";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import SendIcon from "@mui/icons-material/Send";
+import "./Chatcontainer.css";
+import ChatMessage from "./ChatMessage";
+import Picker from "emoji-picker-react";
+import { useParams } from "react-router-dom";
+import db from "../firebase";
+import firebase from "firebase";
