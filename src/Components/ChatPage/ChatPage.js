2eebc2c7dbf6b447bc070fca694c410671f78480
Felix Lai <felix.lai@hotmail.com>
2022-11-23 11:18:10 -0500

    Update ChatPage.js

--- src/Components/ChatPage/ChatPage.js
+++ src/Components/ChatPage/ChatPage.js
@@ -2,3 +2,11 @@ import React from "react";
 import Chatcontainer from "./Chatcontainer";
 import Sidebar from "./Sidebar";
 import "./chatPage.scss";
+
+function ChatPage() {
+  return (
+    <div>ChatPage</div>
+  )
+}
+
+export default ChatPage
